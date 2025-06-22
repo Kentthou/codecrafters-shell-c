01f2d5f8ddd04c1592fad22558d1a8d4e9abd79e
garry <209960306+Kentthou@users.noreply.github.com>
2025-06-23 03:59:39 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main() {
   setbuf(stdout, NULL);  // disables output buffering so we see "$ " right away
 
   char input[100];       // storage for the user's input (up to [100] is 99 characters + null terminator)
+  int echo_char_count = 5;
 
   while (1) {            // infinite loop – keeps the shell running until we break
     printf("$ ");
@@ -17,11 +18,23 @@ int main() {
 
     input[strcspn(input, "\n")] = '\0';
 
-    if (strcmp(input, "exit 0") == 0) {
+    if (strcmp(input, "echo") == 0)
+    {
+      printf("\n");
+    }
+    else if (strncmp(input, "echo ", echo_char_count) == 0)
+    {
+      char *message = input + echo_char_count;
+      printf("%s\n", message);
+    }
+    else if (strcmp(input, "exit 0") == 0) 
+    {
       exit(0);
     }
-
-    printf("%s: command not found\n", input);
+    else
+    {
+      printf("%s: command not found\n", input);
+    }
   }
 
   return 0;
