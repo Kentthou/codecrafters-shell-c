f9e733cb4119e03ee2a6411d0e7d18628ac3d2a3
garry <209960306+Kentthou@users.noreply.github.com>
2025-06-23 04:16:33 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main() {
   setbuf(stdout, NULL);  // disables output buffering so we see "$ " right away
 
   char input[100];       // storage for the user's input (up to [100] is 99 characters + null terminator)
-  int echo_char_count = 5;
+  int cmd_offset = 5;
 
   while (1) {            // infinite loop – keeps the shell running until we break
     printf("$ ");
@@ -22,10 +22,26 @@ int main() {
     {
       printf("\n");
     }
-    else if (strncmp(input, "echo ", echo_char_count) == 0)
+    else if (strncmp(input, "echo ", cmd_offset) == 0)
     {
-      char *message = input + echo_char_count;
+      char *message = input + cmd_offset;
       printf("%s\n", message);
+    }
+    else if (strncmp(input, "type ", cmd_offset) == 0)
+    {
+      char *check_type = input + cmd_offset;
+
+      if (
+        strcmp(check_type, "echo") == 0 ||
+        strcmp(check_type, "exit") == 0 ||
+        strcmp(check_type, "type") == 0
+      ) {
+        printf("%s is a shell builtin\n", check_type);
+      }
+      else 
+      {
+        printf("%s: not found\n", check_type);
+      }
     }
     else if (strcmp(input, "exit 0") == 0) 
     {
