a695a508129fdd6be72a2ef4f156bc7dc1bc5388
garry <209960306+Kentthou@users.noreply.github.com>
2025-06-23 03:15:17 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -2,20 +2,21 @@
 #include <string.h>
 
 int main() {
-
   // Flush after every printf
-
   setbuf(stdout, NULL);
-
-  printf("$ ");
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
 
-  // Remove the trailing newline
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+  printf("$ ");
+
+  // for now, treat all commands as invalid.
+  while(fgets(input, 100, stdin) != NULL)
+  {
+    input[strlen(input) -1] = '\0';
+    printf("%s: command not found\n", input);
+    printf("$ ");
+  }
 
   return 0;
 }
