f92287690f3da44f07d5e374a4cf0169c4dbd2d2
garry <209960306+Kentthou@users.noreply.github.com>
2025-06-25 22:17:38 +0800

    [any message]

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,221 +8,231 @@
 #include <limits.h>
 #include <errno.h>
 
-#define MAX_INPUT 1024
-#define MAX_ARGS 64
+#define MAX_INPUT   128
+#define MAX_ARGS    16
+#define MAX_PATH    PATH_MAX
 
-// Function to decode escape sequences
-char decode_escape(char c) {
-  switch (c) {
-    case 'n': return '\n';
-    case 't': return '\t';
-    case '\\': return '\\';
-    case '\'': return '\'';
-    case '"': return '"';
-    case ' ': return ' ';
-    default:
-      if (c >= '0' && c <= '9') return c;
-      return c;
-  }
+// Check if a command name is one of our built-ins
+int is_builtin(const char *cmd) {
+    return strcmp(cmd, "echo") == 0
+        || strcmp(cmd, "exit") == 0
+        || strcmp(cmd, "pwd")  == 0
+        || strcmp(cmd, "cd")   == 0
+        || strcmp(cmd, "type") == 0;
 }
 
-// Parses input into arguments with escape + quote support
-void parse_input(const char *input, char **args) {
-  int arg_index = 0;
-  int i = 0;
-  int len = strlen(input);
+// Free a NULL-terminated args array
+void free_args(char **args) {
+    for (int i = 0; args[i]; i++) {
+        free(args[i]);
+    }
+}
 
-  while (i < len) {
-    while (i < len && (input[i] == ' ' || input[i] == '\t')) i++;
-    if (i >= len) break;
-
-    char *arg = malloc(MAX_INPUT);
-    int j = 0;
-    int in_single = 0, in_double = 0;
+// Parse input line into args[], handling quotes and backslashes
+void parse_input(char *input, char **args) {
+    int argi = 0, i = 0, len = strlen(input);
 
     while (i < len) {
-      char c = input[i];
+        // skip whitespace
+        while (i < len && (input[i] == ' ' || input[i] == '\t'))
+            i++;
+        if (i >= len) break;
 
-      if (c == '\'' && !in_double) {
-        in_single = !in_single;
-        i++;
-        continue;
-      } else if (c == '"' && !in_single) {
-        in_double = !in_double;
-        i++;
-        continue;
-      } else if (c == '\\') {
-        if (i + 1 < len) {
-          arg[j++] = decode_escape(input[++i]);
-          i++;
-        } else {
-          i++;
+        // start new arg
+        char *buf = calloc(MAX_INPUT, 1);
+        int bi = 0;
+        int in_squote = 0, in_dquote = 0;
+
+        while (i < len) {
+            char c = input[i];
+            // backslash escape
+            if (c == '\\') {
+                if (i + 1 < len) {
+                    buf[bi++] = input[i+1];
+                    i += 2;
+                } else {
+                    // stray backslash: just skip
+                    i++;
+                }
+            }
+            // single-quote toggle
+            else if (c == '\'' && !in_dquote) {
+                in_squote = !in_squote;
+                i++;
+            }
+            // double-quote toggle
+            else if (c == '"' && !in_squote) {
+                in_dquote = !in_dquote;
+                i++;
+            }
+            // unquoted whitespace ends arg
+            else if (!in_squote && !in_dquote && (c == ' ' || c == '\t')) {
+                i++;
+                break;
+            }
+            else {
+                buf[bi++] = c;
+                i++;
+            }
         }
-        continue;
-      } else if (!in_single && !in_double && (c == ' ' || c == '\t')) {
-        i++;
-        break;
-      } else {
-        arg[j++] = c;
-        i++;
-      }
+
+        buf[bi] = '\0';
+        args[argi++] = buf;
+        if (argi >= MAX_ARGS-1) break;
     }
 
-    arg[j] = '\0';
-    args[arg_index++] = arg;
-  }
-
-  args[arg_index] = NULL;
+    args[argi] = NULL;
 }
 
-void free_args(char **args) {
-  for (int i = 0; args[i] != NULL; i++) free(args[i]);
-}
-
-int is_builtin(const char *cmd) {
-  return strcmp(cmd, "echo") == 0 ||
-         strcmp(cmd, "cd") == 0 ||
-         strcmp(cmd, "pwd") == 0 ||
-         strcmp(cmd, "exit") == 0 ||
-         strcmp(cmd, "type") == 0;
-}
-
+// Built-in: echo
 void handle_echo(char **args) {
-  for (int i = 1; args[i]; i++) {
-    printf("%s", args[i]);
-    if (args[i + 1]) printf(" ");
-  }
-  printf("\n");
-}
-
-void handle_pwd() {
-  char cwd[PATH_MAX];
-  if (getcwd(cwd, sizeof(cwd))) {
-    printf("%s\n", cwd);
-  } else {
-    perror("pwd");
-  }
-}
-
-void handle_cd(char **args) {
-  const char *path = args[1];
-  if (!path || strcmp(path, "~") == 0) {
-    path = getenv("HOME");
-    if (!path) {
-      fprintf(stderr, "cd: HOME not set\n");
-      return;
+    for (int i = 1; args[i]; i++) {
+        if (i > 1) fputc(' ', stdout);
+        fputs(args[i], stdout);
     }
-  }
-
-  if (chdir(path) != 0) {
-    fprintf(stderr, "cd: %s: No such file or directory\n", path);
-  }
+    fputc('\n', stdout);
 }
 
+// Built-in: type
 void handle_type(char **args) {
-  if (!args[1]) {
-    fprintf(stderr, "type: missing argument\n");
-    return;
-  }
-
-  if (is_builtin(args[1])) {
-    printf("%s is a shell builtin\n", args[1]);
-    return;
-  }
-
-  char *path_env = getenv("PATH");
-  if (!path_env) {
-    fprintf(stderr, "PATH not set\n");
-    return;
-  }
-
-  char *path = strdup(path_env);
-  char *dir = strtok(path, ":");
-  char full_path[PATH_MAX];
-
-  while (dir) {
-    snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[1]);
-    if (access(full_path, X_OK) == 0) {
-      printf("%s is %s\n", args[1], full_path);
-      free(path);
-      return;
+    if (!args[1]) {
+        fprintf(stderr, "type: missing argument\n");
+        return;
     }
-    dir = strtok(NULL, ":");
-  }
-
-  printf("%s: not found\n", args[1]);
-  free(path);
+    if (is_builtin(args[1])) {
+        printf("%s is a shell builtin\n", args[1]);
+        return;
+    }
+    char *path = getenv("PATH");
+    if (!path) {
+        fprintf(stderr, "type: PATH not set\n");
+        return;
+    }
+    char *pcopy = strdup(path);
+    char *dir = strtok(pcopy, ":");
+    char full[MAX_PATH];
+    int found = 0;
+    while (dir) {
+        snprintf(full, sizeof(full), "%s/%s", dir, args[1]);
+        if (access(full, X_OK) == 0) {
+            printf("%s is %s\n", args[1], full);
+            found = 1;
+            break;
+        }
+        dir = strtok(NULL, ":");
+    }
+    if (!found) {
+        printf("%s: not found\n", args[1]);
+    }
+    free(pcopy);
 }
 
-void run_external(char **args) {
-  char *path_env = getenv("PATH");
-  if (!path_env) {
-    fprintf(stderr, "PATH not set\n");
-    return;
-  }
-
-  char *path = strdup(path_env);
-  char *dir = strtok(path, ":");
-  char full_path[PATH_MAX];
-
-  while (dir) {
-    snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[0]);
-    if (access(full_path, X_OK) == 0) {
-      pid_t pid = fork();
-      if (pid == 0) {
-        execv(full_path, args);
-        perror("execv");
-        exit(1);
-      } else if (pid > 0) {
-        waitpid(pid, NULL, 0);
-        free(path);
-        return;
-      } else {
-        perror("fork");
-        free(path);
-        return;
-      }
-    }
-    dir = strtok(NULL, ":");
-  }
-
-  fprintf(stderr, "%s: command not found\n", args[0]);
-  free(path);
-}
-
-int main() {
-  setbuf(stdout, NULL);
-  char input[MAX_INPUT];
-  char *args[MAX_ARGS];
-
-  while (1) {
-    printf("$ ");
-
-    if (!fgets(input, sizeof(input), stdin)) break;
-    input[strcspn(input, "\n")] = '\0';
-
-    parse_input(input, args);
-    if (!args[0]) continue;
-
-    if (strcmp(args[0], "echo") == 0) {
-      handle_echo(args);
-    } else if (strcmp(args[0], "pwd") == 0) {
-      handle_pwd();
-    } else if (strcmp(args[0], "cd") == 0) {
-      handle_cd(args);
-    } else if (strcmp(args[0], "type") == 0) {
-      handle_type(args);
-    } else if (strcmp(args[0], "exit") == 0) {
-      if (args[1] && strcmp(args[1], "0") == 0) {
-        free_args(args);
-        exit(0);
-      }
+// Built-in: pwd
+void handle_pwd(void) {
+    char cwd[MAX_PATH];
+    if (getcwd(cwd, sizeof(cwd))) {
+        puts(cwd);
     } else {
-      run_external(args);
+        perror("pwd");
+    }
+}
+
+// Built-in: cd
+void handle_cd(char **args) {
+    const char *target = args[1];
+    if (!target || strcmp(target, "~") == 0) {
+        target = getenv("HOME");
+        if (!target) {
+            fprintf(stderr, "cd: HOME not set\n");
+            return;
+        }
+    }
+    char resolved[MAX_PATH];
+    if (!realpath(target, resolved)) {
+        fprintf(stderr, "cd: %s: %s\n", target, strerror(errno));
+        return;
+    }
+    if (chdir(resolved) != 0) {
+        fprintf(stderr, "cd: %s: %s\n", target, strerror(errno));
+    }
+}
+
+// Launch external command via PATH lookup
+void run_external(char **args) {
+    char *path = getenv("PATH");
+    if (!path) {
+        fprintf(stderr, "PATH not set\n");
+        return;
+    }
+    char *pcopy = strdup(path);
+    char *dir = strtok(pcopy, ":");
+    char full[MAX_PATH];
+    int found = 0;
+    while (dir) {
+        snprintf(full, sizeof(full), "%s/%s", dir, args[0]);
+        if (access(full, X_OK) == 0) {
+            found = 1;
+            break;
+        }
+        dir = strtok(NULL, ":");
+    }
+    free(pcopy);
+    if (!found) {
+        fprintf(stderr, "%s: command not found\n", args[0]);
+        return;
     }
 
-    free_args(args);
-  }
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return;
+    }
+    if (pid == 0) {
+        execv(full, args);
+        perror("execv");
+        _exit(1);
+    } else {
+        waitpid(pid, NULL, 0);
+    }
+}
 
-  return 0;
+int main(void) {
+    char line[MAX_INPUT];
+    char *args[MAX_ARGS];
+
+    setbuf(stdout, NULL);
+    while (1) {
+        fputs("$ ", stdout);
+        if (!fgets(line, sizeof(line), stdin)) break;
+        // strip newline
+        line[strcspn(line, "\n")] = '\0';
+
+        parse_input(line, args);
+        if (!args[0]) continue;
+
+        if (strcmp(args[0], "echo") == 0) {
+            handle_echo(args);
+        }
+        else if (strcmp(args[0], "type") == 0) {
+            handle_type(args);
+        }
+        else if (strcmp(args[0], "pwd") == 0) {
+            handle_pwd();
+        }
+        else if (strcmp(args[0], "cd") == 0) {
+            handle_cd(args);
+        }
+        else if (strcmp(args[0], "exit") == 0) {
+            int code = 0;
+            if (args[1]) code = atoi(args[1]);
+            free_args(args);
+            exit(code);
+        }
+        else {
+            run_external(args);
+        }
+
+        free_args(args);
+    }
+    return 0;
 }
